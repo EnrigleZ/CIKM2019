0bdec571f2ed676da9cc84df22db2d4f60e5ae38
ZhouYC <zhouyucong0421@foxmail.com>
2018-12-04 21:43:46 +0800

    modify

---
+++ js/banner.js
@@ -0,0 +1,21 @@
+function openSingleKeydate(id1, id2) {
+    $('#keydate_papers').addClass('hidden');
+    $('#keydate_workshops').addClass('hidden');
+    $('#keydate_tutorials').addClass('hidden');
+    $('#keydate_demos').addClass('hidden');
+
+    $('#a_papers').removeClass('active');
+    $('#a_workshops').removeClass('active');
+    $('#a_tutorials').removeClass('active');
+    $('#a_demos').removeClass('active');
+
+
+    $(id1).removeClass('hidden');
+    $(id2).addClass('active');
+}
+
+jQuery(document).ready(function($) {
+    'use strict';
+
+    openSingleKeydate('#keydate_papers', '#a_papers');
+ });
