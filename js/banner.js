35392860ed047eb1a3549791151265a3703bfedd
ZhouYC <zhouyucong0421@foxmail.com>
2019-03-12 16:18:26 +0800

    carousel update...

--- js/banner.js
+++ js/banner.js
@@ -13,10 +13,25 @@ function openSingleKeydate(id1, id2) {
     $(id1).removeClass('hidden');
     $(id2).addClass('active');
 }
-
+function nextPicture() {
+    var indicators_list = $('.carousel-indicators li');
+    var current_index = indicators_list.index(indicators_list.filter('.active')), l = 5;
+    if (current_index < 0) current_index = Math.floor(Math.random() * 5);
+    
+    indicators_list[(current_index+1)%l].click();
+}
 
 jQuery(document).ready(function($) {
+
     'use strict';
     openSingleKeydate('#keydate_papers', '#a_papers');
+    
+    var rand_index = Math.floor(Math.random() * 5);
+    $('.carousel-indicators li').removeClass('active');
+    $('.carousel .item').removeClass('active').on('click', nextPicture);
+    $('.carousel-indicators li').eq(rand_index).addClass('active');
+    $('.carousel .item').eq(rand_index).addClass('active');
+    
+    window.setInterval("nextPicture()",5000);
     console.log('Welcome to CIKM 2019 website!')
  });
