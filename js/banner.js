d2cef4b9e5b40336874a7fa5e8f0d4564c80d8fa
ZhouYC <zhouyucong0421@foxmail.com>
2019-04-29 19:33:48 +0800

    fix bugs

--- js/banner.js
+++ js/banner.js
@@ -1,22 +1,22 @@
+function openSingleKeydate(id1, id2) {
+    $('#keydate_papers').addClass('hidden');
+    $('#keydate_workshops').addClass('hidden');
+    $('#keydate_tutorials').addClass('hidden');
+    $('#keydate_demos').addClass('hidden');
+
+    $('#a_papers').removeClass('active');
+    $('#a_workshops').removeClass('active');
+    $('#a_tutorials').removeClass('active');
+    $('#a_demos').removeClass('active');
+
+
+    $(id1).removeClass('hidden');
+    $(id2).addClass('active');
+}
 
 jQuery(document).ready(function($) {
     'use strict';
 
-    function openSingleKeydate(id1, id2) {
-        $('#keydate_papers').addClass('hidden');
-        $('#keydate_workshops').addClass('hidden');
-        $('#keydate_tutorials').addClass('hidden');
-        $('#keydate_demos').addClass('hidden');
-    
-        $('#a_papers').removeClass('active');
-        $('#a_workshops').removeClass('active');
-        $('#a_tutorials').removeClass('active');
-        $('#a_demos').removeClass('active');
-    
-    
-        $(id1).removeClass('hidden');
-        $(id2).addClass('active');
-    }
     function nextPicture() {
         var indicators_list = $('.carousel-indicators li');
         var current_index = indicators_list.index(indicators_list.filter('.active')), l = 5;
