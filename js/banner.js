84244ba41090dbeb8f8fba67842f3523d5b3582d
ZhouYC <zhouyucong0421@foxmail.com>
2019-05-14 10:55:41 +0800

    update index info

--- js/banner.js
+++ js/banner.js
@@ -3,11 +3,13 @@ function openSingleKeydate(id1, id2) {
     $('#keydate_workshops').addClass('hidden');
     $('#keydate_tutorials').addClass('hidden');
     $('#keydate_demos').addClass('hidden');
+    $('#keydate_applied').addClass('hidden');
 
     $('#a_papers').removeClass('active');
     $('#a_workshops').removeClass('active');
     $('#a_tutorials').removeClass('active');
     $('#a_demos').removeClass('active');
+    $('#a_applied').removeClass('active');
 
 
     $(id1).removeClass('hidden');
