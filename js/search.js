f1661d751912dfd018561f031d749b3b22f14bc4
Yucong Zhou <zhouyucong0421@foxmail.com>
2019-10-18 21:37:25 +0800

    feat: optimize search

--- js/search.js
+++ js/search.js
@@ -2,27 +2,54 @@ jQuery(document).ready(function($) {
     'use strict';
 
     const infos = $('.info')
+    const html = $('html, body')
+    let results = null
 
     $('#search button').on('click', function() {
-        const str = $('#search input').val().trim().toLowerCase()
         const window_h = $(window).height()
+        html.stop()
 
-        if (str.length === 0) return
+        if (results === null) {
+            results = []
 
-        for (let i = 0; i < infos.length; ++i) {
-            const node = infos.eq(i)
-            const info = node[0].innerText.trim().toLowerCase()
+            const str = $('#search input').val().trim().toLowerCase()
 
-            if (info.indexOf(str) >= 0) {
-                infos.removeClass('highlight')
-                node.addClass('highlight')
-                $('html, body').animate({
-                    scrollTop: node.offset().top - window_h / 2 + node.height() / 2
-                }, 500)
-                return
+            if (str.length === 0) return
+
+
+            for (let i = 0; i < infos.length; ++i) {
+                const node = infos.eq(i)
+
+                if (node[0].innerText.trim().toLowerCase().indexOf(str) >= 0) {
+                    results.push(node)
+                }
             }
+
+            if (results.length === 0) alert('Result not found')
+            else results.current_index = 0
         }
 
-        console.log('Result not found')
+        const { current_index, length } = results
+        if (length === 0) return
+        // if (current_index >= length) current_index = 0
+        const node = results[current_index]
+
+        infos.removeClass('highlight')
+        node.addClass('highlight')
+
+        html.animate({
+            scrollTop: node.offset().top - window_h / 2 + node.height() / 2
+        }, 500)
+
+        results.current_index += 1
+        if (results.current_index === length) results.current_index = 0
+    })
+
+    $('#search input').on('input', function(e) {
+        results = null
+    })
+
+    $('#search input').keypress(function(e) {
+        if (e.keyCode === 13) $('#search button').click()
     })
 })
